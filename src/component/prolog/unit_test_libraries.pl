42b9d4729ab39e3a3e5b81feac71ac9e2241e193
rahilshah13 <rahilshah360@gmail.com>
2026-03-30 01:04:51 -0700

    add additional unit tests

--- src/component/prolog/unit_test_libraries.pl
+++ src/component/prolog/unit_test_libraries.pl
@@ -1,5 +1,6 @@
 :- module(unit_test, [main/1]).
 
+% --- Load all standard Trealla JS libraries ---
 :- use_module(library(lists)).
 :- use_module(library(apply)).
 :- use_module(library(charsio)).
@@ -11,6 +12,14 @@
 :- use_module(library(random)).
 :- use_module(library(crypto)).
 :- use_module(library(assoc)).
+:- use_module(library(ordsets)).
+:- use_module(library(heaps)).
+:- use_module(library(queues)).
+:- use_module(library(clpz)).
+:- use_module(library(lambda)).
+:- use_module(library(wasm)).
+:- use_module(library(pio)).
+:- use_module(library(freeze)).
 
 % --- Descriptive Test Runner ---
 run_test(Category, Name, Goal, test(Name, Status, Category)) :-
@@ -23,28 +32,60 @@ run_test(Category, Name, Goal, test(Name, Status, Category)) :-
 main(Results) :-
     findall(Res, (
         % --- library(lists) ---
-        run_test(lists, 'append/3', append([a], [b], [a, b]), Res)
-        ; run_test(lists, 'select/3', select(2, [1, 2, 3], [1, 3]), Res)
+        run_test(lists, 'member/2', member(x, [a, x, c]), Res)
+        ; run_test(lists, 'reverse/2', reverse([1,2], [2,1]), Res)
+        
         % --- library(apply) ---
-        ; run_test(apply, 'maplist/2', maplist(integer, [1, 2, 3]), Res)
+        ; run_test(apply, 'exclude/3', exclude(integer, [a, 1, b], [a, b]), Res)
+        ; run_test(apply, 'foldl/4', foldl(plus, [1,2,3], 0, 6), Res)
+        
         % --- library(dif) ---
-        ; run_test(dif, 'dif/2 success', dif(X, Y), Res)
-        ; run_test(dif, 'dif/2 constraint', (dif(X, 1), X \= 1), Res)
+        ; run_test(dif, 'dif/2 inequality', (dif(A, B), A = 1, B = 2), Res)
+
         % --- library(charsio) ---
-        ; run_test(charsio, 'read_from_chars/2', read_from_chars("foo.", foo), Res)
+        ; run_test(charsio, 'with_output_to_chars/2', with_output_to_chars(write(hi), "hi"), Res)
+        
         % --- library(dcgs) ---
-        ; run_test(dcgs, 'phrase/2', phrase(("a", "b"), "ab"), Res)
+        ; run_test(dcgs, 'seq//1', phrase(seq("abc"), "abc"), Res)
+
         % --- library(format) ---
-        ; run_test(format, 'format_chars/3', format_chars("~w", [123], "123"), Res)
-        % --- library(js) & library(pseudojson) ---
-        % Note: js_eval/2 requires a JS environment to be active
-        ; run_test(js, 'js_eval/2 string', js_eval("1 + 1", 2), Res)
-        ; run_test(pseudojson, 'json_chars/2', json_chars(JSON, '{"a":1}'), Res)
+        ; run_test(format, 'format/2 to stdout', format("~n", []), Res)
+
+        % --- library(js) ---
+        ; run_test(js, 'js_eval/1 (void)', js_eval("console.log('test')"), Res)
+        ; run_test(js, 'js_fetch/3 (mock)', (catch(js_fetch('https://localhost', _, []), _, true)), Res)
+
+        % --- library(pseudojson) ---
+        ; run_test(pseudojson, 'json_chars/2 object', json_chars(pairs([a-1]), '{"a":1}'), Res)
+
         % --- library(crypto) ---
-        ; run_test(crypto, 'crypto_data_hash/3', crypto_data_hash("abc", _, [algorithm(sha256)]), Res)
+        ; run_test(crypto, 'crypto_password_hash/2', crypto_password_hash("pass", _), Res)
+
         % --- library(random) ---
-        ; run_test(random, 'maybe/0', (maybe ; \+ maybe), Res)
+        ; run_test(random, 'random/1', (random(F), float(F)), Res)
+
         % --- library(assoc) ---
-        ; run_test(assoc, 'empty_assoc/1', empty_assoc(_), Res)
-        ; run_test(assoc, 'put_assoc/4', (empty_assoc(A), put_assoc(k, A, v, _)), Res)
+        ; run_test(assoc, 'get_assoc/3', (list_to_assoc([a-1], Assoc), get_assoc(a, Assoc, 1)), Res)
+
+        % --- library(ordsets) ---
+        ; run_test(ordsets, 'ord_union/3', ord_union([1,3], [2,4], [1,2,3,4]), Res)
+
+        % --- library(heaps) ---
+        ; run_test(heaps, 'heap_add/4', (empty_heap(H), add_to_heap(H, 1, v, _)), Res)
+
+        % --- library(queues) ---
+        ; run_test(queues, 'is_queue/1', (empty_queue(Q), is_queue(Q)), Res)
+
+        % --- library(clpz) ---
+        ; run_test(clpz, 'clpz constraint #=', (X + Y #= 10, X = 7, Y = 3), Res)
+
+        % --- library(lambda) ---
+        ; run_test(lambda, 'lambda call', call(\X^Y^(Y is X + 1), 1, 2), Res)
+
+        % --- library(wasm) ---
+        ; run_test(wasm, 'wasm_print/1', wasm_print(test_output), Res)
+
+        % --- library(freeze) ---
+        ; run_test(freeze, 'freeze/2 delay', (freeze(X, Y = 1), X = 2, Y = 1), Res)
+
     ), Results).
