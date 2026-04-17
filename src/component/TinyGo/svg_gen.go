9b227bf4d61cea5c7b4eaa86c701cb7b9904647a
rahilshah13 <rahilshah360@gmail.com>
2026-04-16 20:20:15 -0700

    update svg

--- src/component/TinyGo/svg_gen.go
+++ src/component/TinyGo/svg_gen.go
@@ -4,6 +4,7 @@ package main
 import (
 	"strings"
 	"syscall/js"
+	"fmt"
 	"github.com/ajstarks/svgo"
 )
 
@@ -11,7 +12,11 @@ func generateSVG(this js.Value, args []js.Value) any {
 	var buf strings.Builder
 	s := svg.New(&buf)
 	s.Start(500, 500)
-	s.Circle(250, 250, 125, "fill:none;stroke:black")
+	for x := 0; x < 500; x += 25 {
+		for y := 0; y < 500; y += 25 {
+			s.Rect(x, y, 23, 23, fmt.Sprintf("fill:rgb(%d,100,%d)", x/2, y/2))
+		}
+	}
 	s.End()
 	return js.ValueOf(buf.String())
 }
