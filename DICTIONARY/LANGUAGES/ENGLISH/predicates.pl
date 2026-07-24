6bb0f7dfd547adf30b6bfbf8db04324de075d53a
rahilshah13 <rahilshah360@gmail.com>
2026-07-23 19:07:14 -0700

    update

--- DICTIONARY/LANGUAGES/ENGLISH/predicates.pl
+++ DICTIONARY/LANGUAGES/ENGLISH/predicates.pl
@@ -1,4 +1,9 @@
-:- module(english, [sentence_len/2, entry_only/1, fill_template/1, avg_word_length/2, words_per_closure/2, sentence_eval/3]).
+:- module(english, [
+    sentence_len/2, entry_only/1, fill_template/1, 
+    avg_word_length/2, words_per_closure/2, sentence_eval/3, 
+    lexical_density/2, semantic_coherence/2, syntactic_entropy/2, readability_score/2,
+    bpe_len/2, ipa_len_val/2
+]).
 
 :- dynamic(entry/4).
 :- use_module(library(dcgs)).
@@ -23,7 +28,7 @@ fill_template([Word|Rest], Seen) :-
 
 sentence_len(L, W) :- entry(W, _, _, _), atom_length(W, L).
 
-% --- New Metric Predicates ---
+% --- Core & Mocked Metric Predicates ---
 
 avg_word_length(ClosureStr, AvgLen) :-
     split_string(ClosureStr, " \t\n\r", " \t\n\r", Words),
@@ -60,6 +65,88 @@ evaluate_sentences([S|Rest], CorIn, IncIn, CorOut, IncOut) :-
         evaluate_sentences(Rest, CorIn, IncNext, CorOut, IncOut)
     ).
 
+% --- Additional Mocked Metrics ---
+
+lexical_density(ClosureStr, Density) :-
+    split_string(ClosureStr, " \t\n\r", " \t\n\r", Words),
+    length(Words, Total),
+    ( Total > 0 ->
+        include(is_long_word, Words, LongWords),
+        length(LongWords, LongCount),
+        Density is LongCount / Total
+    ;   Density = 0.0
+    ).
+
+is_long_word(W) :-
+    atom_length(W, Len),
+    Len > 4.
+
+semantic_coherence(ClosureStr, Coherence) :-
+    atom_length(ClosureStr, Len),
+    Coherence is (Len mod 100) / 100.0.
+
+syntactic_entropy(ClosureStr, Entropy) :-
+    atom_length(ClosureStr, Len),
+    Entropy is (Len mod 50) / 10.0.
+
+readability_score(ClosureStr, Score) :-
+    split_string(ClosureStr, " \t\n\r", " \t\n\r", Words),
+    length(Words, Total),
+    Score is 206.835 - (1.015 * (Total max 1)) - (84.6 * 4.5 / (Total max 1)).
+
+bpe_len(ClosureStr, Len) :-
+    string_codes(ClosureStr, Codes),
+    bpe_code_count(Codes, 0, Len).
+
+bpe_code_count([], Acc, Acc).
+bpe_code_count([C1, C2 | Rest], Acc, Len) :-
+    (   \+ char_type(C1, space), \+ char_type(C2, space)
+    ->  NewAcc is Acc + 1
+    ;   NewAcc is Acc
+    ),
+    bpe_code_count([C2 | Rest], NewAcc, Len).
+bpe_code_count([_], Acc, Acc).
+
+ipa_len_val(ClosureStr, Len) :-
+    ipa_tokens(ClosureStr, Tokens),
+    length(Tokens, TLen),
+    ( TLen > 1 -> Len is TLen - 1 ; Len is 0 ).
+
+ipa_tokens(Str, Tokens) :-
+    atom_string(Atom, Str),
+    atom_codes(Atom, Codes),
+    extract_ipa(Codes, Tokens).
+
+extract_ipa([], []).
+extract_ipa([C|Cs], [Token|Rest]) :-
+    ipamatch([C|Cs], Token, Rem),
+    !,
+    extract_ipa(Rem, Rest).
+extract_ipa([C|Cs], [C|Rest]) :-
+    \+ char_type(C, space),
+    extract_ipa(Cs, Rest).
+extract_ipa([C|Cs], Rest) :-
+    char_type(C, space),
+    extract_ipa(Cs, Rest).
+
+ipamatch(Codes, Token, Rem) :-
+    ipasymbol(Sym),
+    atom_codes(Sym, SymCodes),
+    append(SymCodes, Rem, Codes),
+    atom_string(Token, Sym).
+
+ipasymbol("p"). ipasymbol("b"). ipasymbol("t"). ipasymbol("d").
+ipasymbol("k"). ipasymbol("g"). ipasymbol("m"). ipasymbol("n").
+ipasymbol("ŋ"). ipasymbol("f"). ipasymbol("v"). ipasymbol("θ").
+ipasymbol("ð"). ipasymbol("s"). ipasymbol("z"). ipasymbol("ʃ").
+ipasymbol("ʒ"). ipasymbol("h"). ipasymbol("tʃ"). ipasymbol("dʒ").
+ipasymbol("w"). ipasymbol("j"). ipasymbol("r"). ipasymbol("l").
+ipasymbol("i"). ipasymbol("ɪ"). ipasymbol("e"). ipasymbol("ɛ").
+ipasymbol("æ"). ipasymbol("a"). ipasymbol("ə"). ipasymbol("ʌ").
+ipasymbol("u"). ipasymbol("ʊ"). ipasymbol("o"). ipasymbol("ɔ").
+ipasymbol("ɑ"). ipasymbol("ɒ"). ipasymbol("aɪ"). ipasymbol("eɪ").
+ipasymbol("ɔɪ"). ipasymbol("aʊ"). ipasymbol("oʊ").
+
 % --- Lexical Helpers ---
 noun(W) :- entry(W, n, _, _).
 adj(W)  :- entry(W, adj, _, _).
@@ -78,16 +165,13 @@ verb_phrase --> verb.
 verb_phrase --> verb, noun_phrase.
 
 % --- Determiners (FIXED) ---
-% Allow combinations like: "all the", "my two", etc.
 det_phrase --> predet, det_core.
 det_phrase --> det_core.
 
-% Predeterminers
 predet --> [all].
 predet --> [both].
 predet --> [half].
 
-% Core determiners (mutually exclusive layer)
 det_core --> article.
 det_core --> demonstrative.
 det_core --> possessive.
@@ -97,18 +181,15 @@ det_core --> distributive.
 det_core --> interrogative.
 det_core --> difference.
 
-% Articles
 article --> [the].
 article --> [a].
 article --> [an].
 
-% Demonstratives
 demonstrative --> [this].
 demonstrative --> [that].
 demonstrative --> [these].
 demonstrative --> [those].
 
-% Possessives
 possessive --> [my].
 possessive --> [your].
 possessive --> [his].
@@ -117,7 +198,6 @@ possessive --> [its].
 possessive --> [our].
 possessive --> [their].
 
-% Quantifiers
 quantifier --> [some].
 quantifier --> [any].
 quantifier --> [many].
@@ -128,7 +208,6 @@ quantifier --> [several].
 quantifier --> [most].
 quantifier --> [enough].
 
-% Numbers (minimal; extend as needed)
 number --> [one].
 number --> [two].
 number --> [three].
@@ -136,18 +215,15 @@ number --> [first].
 number --> [second].
 number --> [third].
 
-% Distributives
 distributive --> [each].
 distributive --> [every].
 distributive --> [either].
 distributive --> [neither].
 
-% Interrogatives
 interrogative --> [which].
 interrogative --> [what].
 interrogative --> [whose].
 
-% Difference / choice
 difference --> [other].
 difference --> [another].
 
